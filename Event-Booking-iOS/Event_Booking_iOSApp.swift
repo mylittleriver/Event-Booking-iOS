46391c2de194894a1689325b23500570390e8b59
shujun <shujunxia2-c@my.cityu.edu.hk>
2026-01-18 19:17:28 -0500

    Initial Commit

---
+++ Event-Booking-iOS/Event_Booking_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_Booking_iOSApp.swift
+//  Event-Booking-iOS
+//
+//  Created by 夏姝珺 on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Event_Booking_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
